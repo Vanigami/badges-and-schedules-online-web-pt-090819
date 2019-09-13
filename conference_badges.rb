4012566d69eb74bec33539ac7ec1cb330a8bd7bf
Vanna Winland <vgwinland@gmail.com>
2019-09-13 21:59:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each.map |attendee, index|
-  "Hello, #{attendee}! You'll be assigned to room #{index"
+  attendees.each.map do |attendee, index|
+  "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+end
+end
   
